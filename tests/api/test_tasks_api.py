a368420fa0c6ad78b2b4f14acadb4a11f2d74504
Elena <lenka17.09.03@gmail.com>
2026-09-14 01:29:57 +0300

    Добавила маркеры positive и negative в API-тесты

--- tests/api/test_tasks_api.py
+++ tests/api/test_tasks_api.py
@@ -1,9 +1,12 @@
 import allure
+import pytest
 
 
 @allure.feature("API")
 @allure.story("Задачи")
 @allure.title("Создание задачи")
+@pytest.mark.api
+@pytest.mark.positive
 def test_create_task(api, boards_api, columns_api, tasks_api):
     api.post_project("Проект для задачи")
     boards_api.post_new_board(api.project_id, "Доска")
@@ -18,6 +21,8 @@ def test_create_task(api, boards_api, columns_api, tasks_api):
 
 
 @allure.title("Отметить задачу выполненной")
+@pytest.mark.api
+@pytest.mark.positive
 def test_update_task(api, boards_api, columns_api, tasks_api):
     api.post_project("Проект для задачи")
     boards_api.post_new_board(api.project_id, "Доска")
@@ -33,6 +38,8 @@ def test_update_task(api, boards_api, columns_api, tasks_api):
 
 
 @allure.title("Отправить задачу в архив")
+@pytest.mark.api
+@pytest.mark.positive
 def test_delete_task(api, boards_api, columns_api, tasks_api):
     api.post_project("Проект для задачи")
     boards_api.post_new_board(api.project_id, "Доска")
--- tests/api/test_users_api.py
+++ tests/api/test_users_api.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 from config import BASE_URL
 from api.yougile_api import YougileAPI
 
@@ -6,6 +7,8 @@ from api.yougile_api import YougileAPI
 @allure.feature("API")
 @allure.story("Пользователи")
 @allure.title("Получение текущего пользователя")
+@pytest.mark.api
+@pytest.mark.positive
 def test_get_current_user(token):
     api = YougileAPI(BASE_URL, token)
     response = api.get_users()
